00d7a6411f63107c11b680358f02371ea3c0b983
Fernando Otero <febo@anza.xyz>
2026-01-06 16:44:23 +0000

    Bump pinocchio version (#1)
    * Bump version
    
    * Use release version

--- programs/pinocchio/src/lib.rs
+++ programs/pinocchio/src/lib.rs
@@ -1,7 +1,7 @@
 //! Simple pinocchio program.
 
-use pinocchio::pubkey::Pubkey;
+use pinocchio::Address;
 
 mod entrypoint;
 
-pub const PROGRAM_ID: Pubkey = [2; 32];
+pub const PROGRAM_ID: Address = Address::new_from_array([2; 32]);
