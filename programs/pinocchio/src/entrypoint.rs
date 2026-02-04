b22f9cd4d11bc9045eef05a3156d29ab806e0ba9
febo <febo@anza.xyz>
2026-02-04 22:05:41 +0000

    [wip]: Add entrypoint

--- programs/pinocchio/src/entrypoint.rs
+++ programs/pinocchio/src/entrypoint.rs
@@ -1,47 +1,56 @@
-//use pinocchio::error::ProgramError;
-use pinocchio::{entrypoint, AccountView, Address, ProgramResult};
-//use pinocchio_system::instructions::CreateAccount;
-//use solana_program_log::log;
+use core::{mem::size_of, ptr::with_exposed_provenance_mut, slice::from_raw_parts};
+use pinocchio::{AccountView, Address, ProgramResult};
 
-// Declares the entrypoint of the program.
-entrypoint!(process_instruction);
+/// `assert_eq(core::mem::align_of::<u128>(), 8)` is true for BPF but not
+/// for some host machines.
+const BPF_ALIGN_OF_U128: usize = 8;
 
-/// Instruction processor
+/// Align a pointer to the BPF alignment of [`u128`].
+macro_rules! align_pointer {
+    ($ptr:ident) => {
+        // Integer-to-pointer cast: first compute the aligned address as a `usize`,
+        // since this is more CU-efficient than using `ptr::align_offset()` or the
+        // strict provenance API (e.g., `ptr::with_addr()`). Then cast the result
+        // back to a pointer. The resulting pointer is guaranteed to be valid
+        // because it follows the layout serialized by the runtime.
+        with_exposed_provenance_mut(
+            ($ptr.expose_provenance() + (BPF_ALIGN_OF_U128 - 1)) & !(BPF_ALIGN_OF_U128 - 1),
+        )
+    };
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn entrypoint(program_input: *mut u8, instruction_data: *mut u8) -> u64 {
+    // First 8-bytes of program_input contains the number of accounts.
+    let accounts = program_input as *mut u64;
+
+    // The 8-bytes before the instruction data contains the length of
+    // the instruction data, even if the instruction data is empty.
+    let ix_data_len = *(instruction_data.sub(size_of::<u64>()) as *mut u64) as usize;
+
+    // The program_id is located right after the instruction data.
+    let program_id = &*(instruction_data.add(ix_data_len) as *const Address);
+
+    // The slice of account pointers is located right after the program_id.
+    let slice_ptr = instruction_data.add(ix_data_len + size_of::<Address>());
+    let accounts = from_raw_parts(
+        align_pointer!(slice_ptr) as *const AccountView,
+        *accounts as usize,
+    );
+
+    // The instruction data slice.
+    let instruction_data = from_raw_parts(instruction_data, ix_data_len);
+
+    match process_instruction(program_id, accounts, instruction_data) {
+        Ok(_) => 0,
+        Err(e) => e.into(),
+    }
+}
+
 pub fn process_instruction(
     _program_id: &Address,
     _accounts: &[AccountView],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    // (1) run_accounts
     core::hint::black_box(Ok(()))
-    // end of 1
-
-    // (2) run_cpi
-    /*
-    let [from, to, _system_program] = accounts else {
-        return Err(ProgramError::NotEnoughAccountKeys);
-    };
-
-    CreateAccount {
-        from,
-        to,
-        lamports: 1_000_000_000,
-        space: 10,
-        owner: program_id,
-    }
-    .invoke()
-    */
-    // end of 2
-
-    // (3) run_log
-    /*
-    let [account] = accounts else {
-        return Err(ProgramError::NotEnoughAccountKeys);
-    };
-
-    log!("lamports={}", &[account.lamports(), account.lamports()]);
-
-    Ok(())
-    */
-    // end of 3
 }
